46768e79d2b3b570396b48f3e0cb4fe6354e3940
B-thanybyrd <git@ownyourhill.com>
2021-11-18 21:05:28 -0600

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const choices = ['rock', 'paper', 'scissors']
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gameCount: 0,
+            winHistory: 0,
+            playerChoice: null,
+            computerChoice: null,
+            winner: null
+        };
+    }
+
+    calculateWinner() {
+        let gameCount = this.state.gameCount;
+        gameCount++
+        let winHistory = this.state.winHistory;
+        const playerChoice = this.state.playerChoice;
+        const computerChoice = this.state.computerChoice;
+        
+        let winner;
+
+        switch(playerChoice + computerChoice) {
+            case 'rockrock':
+            case 'scissorsscissors':
+            case 'paperpaper':
+                winner = `It's a draw!`
+            break;
+            case 'rockscissors':
+            case 'scissorspaper':
+            case 'paperrock':
+                winner = 'You win!' 
+                break;
+            default: 
+                winner = 'You lose.'
+                break;
+        } 
+
+        this.setState({
+            gameCount: gameCount,
+            winner: winner
+            // TODO: Add history - if player wins >= half games, winner
+        });
+    }
+    
+    handleClick(choice) {
+        this.setState({
+            playerChoice: choice,
+            computerChoice: choices[Math.floor(Math.random() * choices.length)],
+        });
+        this.calculateWinner(this.playerChoice, this.computerChoice)
+    }
+
+    render() {
+        return (
+            <div className="game-board">
+                <div className="info">
+                    <h2>Your Move: {this.state.playerChoice}</h2>
+                    <h2>Computer Move: {this.state.computerChoice}</h2>
+                    <p>Winner: {this.state.winner}</p>
+                    <p>Game: {this.state.gameCount}</p>
+                </div>
+                <div className="buttons">
+                    {choices.map((choice, index) =>
+                        <button key={index} onClick={() => this.handleClick(choice)}>
+                            {choice}
+                        </button>
+                    )}
+                </div> 
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Game
